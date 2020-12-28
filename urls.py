6d52c4ce38d3ec7ae8ab20294dac2bb05fae61d6
Meryem L <meryemlaouinate@gmail.com>
2020-12-28 22:24:55 +0100

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,11 +1,14 @@
-
-from django.contrib import admin
 from django.urls import path, include
-from MyAPI import views
+from . import views
+from rest_framework import routers
 
+router = routers.DefaultRouter()
+router.register('MyAPI', views.ApprovalsView)
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('MyAPI.urls')),
-    
+    path('api/', include(router.urls)),
+    path('status/', views.approvereject),
+    path('form/', views.cxcontact, name='cxform'),
 
-]
+
+ 
+] 
